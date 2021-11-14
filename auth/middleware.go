dc4177e0d363c6f75eb0a879bd7bbf61f186a4a6
nickolation <sf.1951@mail.ru>
2021-11-14 18:25:32 +0300

    Add signUp logic; imp middleware worker-functs; structed erorrs;

--- auth/middleware.go
+++ auth/middleware.go
@@ -3,31 +3,44 @@ package auth
 import (
 	"fmt"
 	"strings"
+
+	sdk "github.com/nickolation/pointsalvor"
 )
 
 //custom auth mod --> zap
 const (
 	authIdentity = "auth"
+	authKey      = "auth!!chat_id:%s!!project_id:%d"
+	authTable    = "table:[%s]"
 )
 
 //custom errors --> zap
 var (
+	//parse key
 	errNilKey     = fmt.Errorf("key is nil: parse isn'd valid operation")
 	errInvalidKey = fmt.Errorf("key isn't valid structure: parse isn'd valid operation")
 	errForeignKey = fmt.Errorf("key is foreign for auth-data: parse isn'd valid operation")
 	errNilId      = fmt.Errorf("id in key-string is nil: parse isn'd valid operation")
 
+	//auth
 	errAuth      = fmt.Errorf("internal auth error: auth isn' valid ")
-	errNilChatId = fmt.Errorf("chatId is nil: auth locked!")
+	errNilChatId = fmt.Errorf("chatId is nil: auth locked")
+
+	//registr
+	errNilOpt    = fmt.Errorf("Keyopt data is nil: registration locked")
+	errIdProject = fmt.Errorf("Id project is nil: registration locked")
 )
 
 //Storage with part of key auth-data: auth!!...
-type KeypartStorage struct {
+type KeyTokenOpt struct {
 	//identitty of user
 	chatId string
 
 	//used in set-handlers for identity of entities
 	projId string
+
+	//todoist token required for performing the request to api
+	token string
 }
 
 //Status authentification used in validate middleware method
@@ -35,12 +48,37 @@ type KeypartStorage struct {
 type StatusAuth struct {
 	//status of authorization
 	//true is auth - false against
-	Status bool
+	status bool
+}
+
+//makes new pointsalvor-agent with token is need to use sdk-method
+func linkAgent(token string) (*sdk.Agent, error) {
+	ag, err := sdk.NewAgent(token)
+	if err != nil {
+		return nil, err
+	}
+
+	return ag, nil
+}
+
+//make name of table
+//used in signUp method for sending to sdk
+func callTable(chatId string) string {
+	return fmt.Sprintf(authTable, chatId)
+}
+
+//make auth-key value: auth!!chat_id:<>!!projId:<>
+func makeKey(chatId string, projId int) (string, error) {
+	if projId == 0 {
+		return "", errIdProject
+	}
+
+	return fmt.Sprintf(authKey, chatId, projId), nil
 }
 
 //key allows the struct: auth!chat_id:<...>!!proj_id:<...>
 //parse key to custom struct contains need parts
-func ParseAuthKey(key string) (*KeypartStorage, error) {
+func parseAuthKey(key string) (*KeyTokenOpt, error) {
 	if key == "" {
 		return nil, errNilKey
 	}
@@ -67,7 +105,7 @@ func ParseAuthKey(key string) (*KeypartStorage, error) {
 		return nil, errNilId
 	}
 
-	return &KeypartStorage{
+	return &KeyTokenOpt{
 		chatId: chatId,
 		projId: projId,
 	}, nil
@@ -75,21 +113,21 @@ func ParseAuthKey(key string) (*KeypartStorage, error) {
 
 //Validate user for existing the key:auth!! with same chat_id
 //Doesn't lock the process but make statusAuth: status - false
-func IsAuth(key string, chatId string) StatusAuth {
-	parts, err := ParseAuthKey(key)
+func isAuth(key string, chatId string) StatusAuth {
+	parts, err := parseAuthKey(key)
 	if err != nil {
 		return StatusAuth{
-			Status: false,
+			status: false,
 		}
 	}
 
 	if parts.chatId == chatId {
 		return StatusAuth{
-			Status: true,
+			status: true,
 		}
 	}
 
 	return StatusAuth{
-		Status: false,
+		status: false,
 	}
 }
--- auth/repository.go
+++ auth/repository.go
@@ -5,21 +5,25 @@ import (
 	"log"
 
 	"github.com/go-redis/redis/v8"
+	sdk "github.com/nickolation/pointsalvor"
 )
 
 type authRepoInterface interface {
 	//sign-in
-	searchAgent(ctx context.Context, db *redis.Client, chatId string) error
+	searchAgent(ctx context.Context, db *redis.Client, chatId string) (string, error)
 
 	//foreign sign-in
 	foreignAuth()
 
 	//sign-up
-	casheAgent()
+	casheAgent(ctx context.Context, db *redis.Client, chatId string, ag *sdk.Agent) error
 }
 
 type authRepo struct {
 	db *redis.Client
+
+	//pointsalvor agent for toodist-requests
+	ag *sdk.Agent
 }
 
 func newAuthRepo(db *redis.Client) *authRepo {
@@ -28,29 +32,89 @@ func newAuthRepo(db *redis.Client) *authRepo {
 	}
 }
 
-func (ar *authRepo) searchAgent(ctx context.Context, db *redis.Client, chatId string) error {
+//search agent in database by key
+//cheking the auth status of user and selecting the todoist-token
+func (ar *authRepo) searchAgent(ctx context.Context, db *redis.Client, chatId string) (string, error) {
 	if chatId == "" {
-		return errNilChatId
+		return "", errNilChatId
 	}
 
 	iter := db.Scan(ctx, 0, "auth*", 0).Iterator()
 	if err := iter.Err(); err != nil {
-		return err
+		return "", err
 	}
 
 	for iter.Next(ctx) {
 		key := iter.Val()
-		status := IsAuth(key, chatId)
+		status := isAuth(key, chatId).status
 
-		if status.Status {
+		if status {
+			//auth succes log
 			log.Printf("auth is succes: chat_id - [%s]", chatId)
-			return nil
+
+			token, err := db.Get(ctx, key).Result()
+			if err != nil {
+				return "", err
+			}
+
+			return token, nil
 		}
 	}
 
-	return errAuth
+	return "", errAuth
 }
 
 func (ar *authRepo) foreignAuth() {}
 
-func (ar *authRepo) casheAgent() {}
+//cashe agent in database according to the auth key-value scheme
+//validation opt inner data on nillable
+func (ar *authRepo) casheAgent(ctx context.Context, db *redis.Client, opt *KeyTokenOpt) error {
+	var (
+		token  = opt.token
+		chatId = opt.chatId
+	)
+
+	if chatId == "" || token == "" {
+		return errNilOpt
+	}
+
+	//link agent with same token
+	ag, err := linkAgent(token)
+	if err != nil {
+		return err
+	}
+
+	//make table
+	var tableName = callTable(chatId)
+
+	proj, err := ag.AddProject(ctx, tableName)
+	if err != nil {
+		return err
+	}
+
+	//make key for auth: key- value
+	key, err := makeKey(chatId, proj.Id)
+	if err != nil {
+		return nil
+	}
+	//log-key
+	log.Printf("key - [%s]", key)
+
+	//make pair in redis
+	if err = db.Set(ctx, key, token, 0).Err(); err != nil {
+		return err
+	}
+
+	//test-logic
+	val, err := db.Get(ctx, key).Result()
+	if err != nil {
+		//log key-value
+		log.Printf("token value for key [%s] - [%s]", key, val)
+		return err
+	}
+	//same log
+	log.Printf("token value for key [%s] - [%s]", key, val)
+	//test-logic
+
+	return nil
+}
